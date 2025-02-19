622e08b656b12d3d00694640a91815c9e3840ef1
mmtzitziras <mariostzitziras4@gmail.com>
2025-02-19 15:38:01 +0200

    Added basic style, navbar and backround

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './styles.css';
+
+document.addEventListener("DOMContentLoaded", function() {
+    const navbar = document.querySelector('.navbar');
+    const bgColor = window.getComputedStyle(navbar).backgroundColor;
+    const rgb = bgColor.match(/\d+/g);
+    const brightness = Math.round(((parseInt(rgb[0]) * 299) +
+                                  (parseInt(rgb[1]) * 587) +
+                                  (parseInt(rgb[2]) * 114)) / 1000);
+    const textColor = (brightness > 125) ? 'black' : 'white';
+    document.documentElement.style.setProperty('--text-color', textColor);
+});
