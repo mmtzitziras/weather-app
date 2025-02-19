80134aaff4da2e73c56c47726eb34ec8f17de055
mmtzitziras <mariostzitziras4@gmail.com>
2025-02-19 15:58:58 +0200

    Added placeholder weather cards and function to fetch data from api

--- src/index.js
+++ src/index.js
@@ -10,3 +10,16 @@ document.addEventListener("DOMContentLoaded", function() {
     const textColor = (brightness > 125) ? 'black' : 'white';
     document.documentElement.style.setProperty('--text-color', textColor);
 });
+
+getWeather();
+
+async function getWeather() {
+    try{
+        const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Thessaloniki?unitGroup=metric&key=HVYUN4KRD4DB253H2P34LKNF4&contentType=json');
+        const data = await response.json();
+        console.log(data);
+    }
+    catch(error){
+        console.error(error);
+    }
+}
