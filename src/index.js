15d07a94864a2c03dce36f2cbd7cf8b091a53934
mmtzitziras <mariostzitziras4@gmail.com>
2025-02-19 18:26:03 +0200

    The user can now search for a city and display the weather forecast

--- src/index.js
+++ src/index.js
@@ -11,15 +11,36 @@ document.addEventListener("DOMContentLoaded", function() {
     document.documentElement.style.setProperty('--text-color', textColor);
 });
 
-getWeather();
+
 
 async function getWeather() {
     try{
-        const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Thessaloniki?unitGroup=metric&key=HVYUN4KRD4DB253H2P34LKNF4&contentType=json');
+        const locationName = document.getElementById('locationName').value.toLowerCase();
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationName}?unitGroup=metric&key=HVYUN4KRD4DB253H2P34LKNF4&contentType=json`);
         const data = await response.json();
         console.log(data);
+        const currentConditions = document.getElementById('currentConditions');
+        const currentTemperature = document.getElementById('currentTemperature');
+        const currentAirPressure = document.getElementById('currentAir');
+
+        currentConditions.innerHTML = `Conditions: ` + data.currentConditions.conditions;
+        currentTemperature.innerHTML = `Temperature: ` + data.currentConditions.temp + `°C`;
+        currentAirPressure.innerHTML = `Wind: ` + data.currentConditions.windspeed + ` kh/h`;
+
+         // Update forecast weather
+         const forecastCards = document.querySelectorAll('.forecast-weather-cards .weather-card');
+
+         data.days.slice(1, 7).forEach((day, index) => {
+             const forecastCard = forecastCards[index];
+             forecastCard.querySelector('h2').innerText = new Date(day.datetime).toLocaleDateString('en-US', { weekday: 'long' });
+             forecastCard.querySelector('p:nth-child(2)').innerText = `${day.temp}°C`;
+             forecastCard.querySelector('p:nth-child(3)').innerText = `Conditions: ${day.description}`;
+         });
+
     }
     catch(error){
         console.error(error);
     }
 }
+
+window.getWeather = getWeather;
